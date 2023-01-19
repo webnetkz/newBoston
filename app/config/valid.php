0edee3fbe5a1883e4eb5cac0aead928ee71e6894
webnetkz <dbqqbq@gmail.com>
2023-01-19 12:45:25 +0600

    Стартовая контрольная точка

--- app/config/valid.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    if(!empty($_SESSION['name'])) {
-        header('Location: ../profile.php');
-    }
--- public/js/closeError.js
+++
@@ -1,5 +0,0 @@
-// Скрытие ошибки
-function closeError() {
-    var er = document.querySelector('.visibleEr');
-    er.setAttribute('style', 'display: none;');
-}
